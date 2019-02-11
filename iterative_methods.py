d6741a582382c9dc7d3e1c3b7c7382f6f8e9675b
Harsh Raj <harshraj11584@gmail.com>
2019-02-11 22:43:30 +0530

    Add files via upload

--- iterative_methods.py
+++ iterative_methods.py
@@ -1,7 +1,10 @@
-from __future__ import division
-from IPython.display import display, Math, Latex
+#MA17BTECH11003
+# from __future__ import division
+# from IPython.display import display, Math, Latex
 from sympy import *
+import sys
 import numpy as np
+sys.displayhook = pprint
 init_printing()
 print('\n\nInitial Setup Complete')
 
@@ -217,6 +220,90 @@ def newton_raphson(expression , f):
 		print "	Root = ",root
 		print "	f(Root) = ",f(root)
 
+
+def birge_vieta(expression,f):
+	print "Starting Birge Vieta Method"
+	print "\nPn(x) = \n" 
+	pprint(expression)	
+	Pn = Poly(expression,x)
+	a = []*degree(Pn, gen=x)
+	a = Pn.all_coeffs()
+	print "\na_0, a_1, a_2, ... a_"+str(degree(Pn,gen=x))+" are: ",a,"\n"
+	p = Symbol('p')
+	Qn, R = div(Pn,(x-p))
+	print "\nb_n=\n\n",
+	pprint(R.as_expr())
+	b_n = R
+	c_n_minus_1 = diff(b_n,p)
+
+	p0 = float(input("\nEnter initial approximation p0: "))
+	err = float(input("Enter error tolerance : "))
+	prev_root = 0 ; new_root = p0
+	iter_count = 0 ; not_converging = 0
+	while(abs(N(new_root-prev_root))>err):
+		iter_count = iter_count + 1
+		print "Iteration No. ", iter_count, ": "
+		tmp = new_root
+		if (float(N((c_n_minus_1.subs([(x,1),(p,new_root)]))))==0):
+			print ("Not Converging\n")
+			not_converging = 1
+			break;
+		new_root = new_root-float(N(b_n.subs([(x,1),(p,new_root)])))/float(N((c_n_minus_1.subs([(x,1),(p,new_root)]))))
+		#print b_n.degree_list()
+		#this = float(N(b_n.subs([(x,1),(p,new_root)])))
+		#print this
+		#print type(this)
+		prev_root = tmp
+		print "	x"+str(iter_count)+" = ",new_root
+		print "	f(x"+str(iter_count)+") = ",f(new_root)
+
+	if (not_converging==0):
+		print "\n\nRoot Approximated within error range."
+		print "Number of Iterations = ",iter_count
+		print "	Root = ",new_root
+		print "	f(Root) = ",f(new_root)
+
+def bairstow(expression,f):
+	print "Starting Bairstow's Method"
+	print "\nPn(x) = " 
+	pprint(expression)
+	Pn = Poly(expression,x)	
+	p = Symbol('p')
+	q = Symbol('q')
+	quad_div = Poly(sympify('x**2 + p*x+q'),x)
+	print "Suppose this quadratic polynomial divides Pn(x) : "
+	pprint (sympify('x**2 + p*x+q'))
+	Qn, Rem = div(Pn,quad_div)
+	p0,q0 = 0,0
+	p1,q1 = map(float, raw_input("\nEnter initial guess p,q: ").split())
+	err = float(input("Enter error tolerance : "))
+	print("Remainder is : ",Rem)
+	R,S = div(Rem,x)
+	print("R=",R,"S=",S)
+	Rp = diff(R,p)
+	Rq = diff(R,q)
+	Sp = diff(S,p)
+	Sq = diff(S,q)	
+	del_p = - (R*Sq - S*Rq)/(Rp*Sq - Rq*Sp)
+	del_q = - (Rp*S - Sp*R)/(Rp*Sq - Rq*Sp)
+	iter_count = 0
+	while abs(p1-p0)>err or abs(q1-q0)>err : 
+		pt,qt = p1,q1
+		p_inc =  float(N(del_p.subs([(x,1),(p,p1),(q,q1)])))
+		q_inc =  float(N(del_q.subs([(x,1),(p,p1),(q,q1)])))
+		p1 = p1 + p_inc
+		q1 = q1 + q_inc
+		p0,q0 = pt,qt
+		iter_count = iter_count + 1
+		print "Iteration No. ", iter_count, ": "
+		print "	p"+str(iter_count)+" = ",p1
+		print "	q"+str(iter_count)+" = ",q1
+	print "\n\nFound.\nQuadratic Equation that divides Pn(x) is \n"
+	quad_div = sympify("x**2+"+str(p1)+"*x+"+str(q1))
+	pprint(quad_div)
+	print "\n\nRoots are ",solve(quad_div, x)
+
+
 #Syntax Constraints
 print("""		
 	Syntax Constraints for entering function - 
@@ -244,9 +331,12 @@ print("""
 		""")
 #example s is given
 #s = '-2*((-x+1/x)/(x*(x-1/x)**2)-1/(x*(x-1/x)))'
+#s="(7/2)*x**3 - x**9 +1"
+#s="6*x**5+11*x**4 -33*x**3-33*x**2 + 11*x+6"
 x = Symbol('x')
 s=raw_input("Enter a function :  ").strip()
 expression=sympify(s)
+
 #will lambdify expression for fast parrallel multipoint computation 
 f = lambdify(x, expression, "numpy")
 #print("Verify f(0.9)")
@@ -254,20 +344,26 @@ f = lambdify(x, expression, "numpy")
 
 try_again=1
 while(try_again==1):
-	print "\nf(x) = ",expression
+	print("\nf(x) = \n")
+	pprint(expression)
 	f_dash = diff(expression,x)
-	print "df(x)/dx = " , f_dash
+	print "\ndf(x)/dx = \n"
+	pprint(f_dash)
 	print """Select Index of Iterative Method:
 		 1. Bisection Method
 		 2. Secant Method
 		 3. Regula-Falsi Method
 		 4. Newton-Raphson Method
+		 5. Birge-Vieta Method
+		 6. Bairstow's Method
 				 """
 	choice_of_method = input()
 	if (choice_of_method==1):		bisection_method(f)
 	elif (choice_of_method==2):		secant_method(f)
 	elif (choice_of_method==3):		regula_falsi(f)
 	elif (choice_of_method==4):		newton_raphson(expression,f)
+	elif (choice_of_method==5):		birge_vieta(expression,f)
+	elif (choice_of_method==6): 	bairstow(expression,f)
 	try_again=0
 	k = (raw_input("Try another method? [Y/N] : ")).strip()
 	try_again = 1 if (k=='y' or k=='Y') else 0
